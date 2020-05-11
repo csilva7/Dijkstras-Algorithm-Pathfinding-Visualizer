e46c16bc10a1782edc4862fab7c02c0f09416ce6
catarina <katesilva@icloud.com>
2020-05-11 19:28:10 +0100

    add untracked

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PathfindingVisualiser from "./PathfindingVisualiser/PathfindingVisualiser";
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualiser></PathfindingVisualiser>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PathfindingVisualiser/PathfindingVisualiser.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Node from "./Node/Node";
+import ".PathfindingVisualiser.css";
+
+export default class PathfindingVisualiser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+    };
+  }
+
+  render() {
+    //testing
+  }
+}
